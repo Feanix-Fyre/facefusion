35fa6be454acb6e7e876e3c8ba96eab02d8c4769
Feanix-Fyre <24631543+Feanix-Fyre@users.noreply.github.com>
2023-08-23 08:28:44 -0400

    Update globals.py

--- facefusion/globals.py
+++ facefusion/globals.py
@@ -28,4 +28,4 @@ max_memory : Optional[int] = None
 execution_providers : List[str] = []
 execution_thread_count : Optional[int] = None
 execution_queue_count : Optional[int] = None
-share : Optional[bool] = None
+share : Optional[str] = None
